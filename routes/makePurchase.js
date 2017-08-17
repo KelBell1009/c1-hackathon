ec91a88dca21c17a88e7169c4f60aeb30c949de7
David Van <David@DavidVan.net>
2017-08-17 13:42:54 -0400

    Updated generatePurchase.

--- routes/makePurchase.js
+++ routes/makePurchase.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const request = require('superagent');
 const router = express.Router();
 
 // Handle GET request for index page.
-router.post('/', function(req, res, next) {
-    console.log(req.body);
-    res.json(req.body);
+router.post('/', (req, res, next) => {
+    request.post('http://api.reimaginebanking.com/accounts/5995b1f0ceb8abe2425184a5/purchases?key=4e7fe49f0f5ba28b8c28032a43551d7b').send(req.body).end((err, res2) => {
+        res.json(res2);
+    });
 });
 
 module.exports = router;
