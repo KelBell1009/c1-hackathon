b4042cf19a79d9d30ae1a415eb384578b94aa2d2
Kelly Zhang <kelly3zhang@gmail.com>
2017-08-17 15:18:16 -0400

    subset of merchants for demo

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bootstrap = require('bootstrap');
 const router = express.Router();
 
 // Handle GET request for index page.
 router.get('/', function(req, res, next) {
     res.render('index.html');
 });
+
+
 
 module.exports = router;
