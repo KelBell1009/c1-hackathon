e7d79af605239ec7fff66453f7ab6cee45f697ee
Justin Kuang <zk65@cornell.edu>
2017-08-17 13:42:17 -0400

    finished ability to buy random stuff

--- app.js
+++ app.js
@@ -34,6 +34,34 @@ function urlWrap(input) {
 var customerID = "5995a81dceb8abe24251849e"; //Donald Trump's ID
 var request = require('superagent');
 
+function buyRandStuff(accountID, number_of_items) {
+    request.get(urlWrap("accounts/" + accountID)).end(function (err, res) {
+        var accountName = "";
+        var accountType = "";
+        if (err || !res.ok) {
+            console.log('Oh no! error');
+        } else {
+            accountName = res.body.nickname;
+            accountType = res.body.type;
+        }
+        // console.log(res.body);
+        for (var i = 0; i < number_of_items; i++) {
+            var singleMerch = merchants.results[i];
+            console.log(singleMerch);
+            request.post(urlWrap("accounts/" + accountID + "/purchases")).send({
+                "merchant_id": singleMerch._id,
+                "medium": "balance",
+                "purchase_date": "2017-08-17",
+                "amount": 5,
+                "description": accountType + "," + accountName + "," + singleMerch.name
+            }).end();
+        }
+    });
+
+
+}
+// buyRandStuff("5995cce8ceb8abe242519650", 5);
+
 // request.get(urlWrap("customers")).end(function (err, res) {
 //     if (err || !res.ok) {
 //         console.log('Oh no! error');
@@ -42,13 +70,14 @@ var request = require('superagent');
 //     }
 // });
 
-request.post("http://api.reimaginebanking.com/customers/5995a81dceb8abe24251849e/accounts?key=4e7fe49f0f5ba28b8c28032a43551d7b").send({
-    "type": "Savings",
-    "nickname": "save",
+request.post(urlWrap("customers/" + customerID + "/accounts")).send({
+    "type": "Credit Card",
+    "nickname": "Bob",
     "rewards": 0,
-    "balance": 10000,
+    "balance": 500000,
     "account_number": "5185139221386420"
 }).end();
+
 // request.get(urlWrap("customers")).end(function (err, res) {
 //     if (err || !res.ok) {
 //         console.log('Oh no! error');
@@ -56,7 +85,9 @@ request.post("http://api.reimaginebanking.com/customers/5995a81dceb8abe24251849e
 //         console.log('yay got ' + JSON.stringify(res.body));
 //     }
 // });
+
 // console.log(urlWrap("customers/" + customerID + "/accounts"));
+
 // request.post().send({
 //         "first_name": "Donald",
 //         "last_name": "Trump",
