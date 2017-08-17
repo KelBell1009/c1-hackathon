77ab5506f858cc2ac6c5e6eb14fbb97dbcecfea5
Justin Kuang <zk65@cornell.edu>
2017-08-17 10:53:57 -0400

    working post request

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const express = require('express')
 const app = express()
-
-
-
-
-// app.get('/', function (req, res) {
-//     res.send('Hello World!')
-// })
-
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
 
+var key = "?key=4e7fe49f0f5ba28b8c28032a43551d7b";
+var category = "customers";
+var urlFirst = "http://api.reimaginebanking.com/";
+
+// var request = require('superagent');
 
 var request = require('superagent');
-request.get('http://api.reimaginebanking.com/atms?key=4e7fe49f0f5ba28b8c28032a43551d7b').end(function (err, res) {
-    if (err || !res.ok) {
-        console.log('Oh no! error');
-    } else {
-        console.log('yay got ' + JSON.stringify(res.body));
-    }
-});
+
+// request.get('http://api.reimaginebanking.com/atms?key=4e7fe49f0f5ba28b8c28032a43551d7b').end(function (err, res) {
+//     if (err || !res.ok) {
+//         console.log('Oh no! error');
+//     } else {
+//         console.log('yay got ' + JSON.stringify(res.body));
+//     }
+// });
+
+var url = urlFirst + category + key;
+console.log(url);
+
+
+request.post(url).send({
+        "first_name": "Donald",
+        "last_name": "Trump",
+        "address": {
+            "street_number": "1600",
+            "street_name": "Pennsylvania Ave",
+            "city": "Washington",
+            "state": "DC",
+            "zip": "20500"
+        }
+    })
+    .end();
