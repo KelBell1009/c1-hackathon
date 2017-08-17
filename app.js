90e8e418a457b450f18f54a211c727bb97423ff5
David Van <David@DavidVan.net>
2017-08-17 10:50:44 -0400

    Added basic structure.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
+const index = require('./routes/index'); // Handle our requests in routes/index.js
 
+app.use(express.static(path.join(__dirname, 'public'))); // Expose the public folder
+app.set('views', path.join(__dirname, 'views')); // Specify our view folder explicitly
 
-// app.get('/', function (req, res) {
-//     res.send('Hello World!')
-// })
+app.use('/', index); // Serve the index page.
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+    console.log('Started on port 3000!');
+});
 
 
 var request = require('superagent');
