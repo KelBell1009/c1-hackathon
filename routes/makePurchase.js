f675c8b9c88e39c02aaa078be05aa77d10b9f3d0
David Van <David@DavidVan.net>
2017-08-17 17:41:26 -0400

    Added RESTful endpoints to allow CRUD operations on splurge list.

--- routes/makePurchase.js
+++ routes/makePurchase.js
@@ -3,9 +3,9 @@ const request = require('superagent');
 const router = express.Router();
 const utils = require('../nessieUtils').use('4e7fe49f0f5ba28b8c28032a43551d7b', '5995a81dceb8abe24251849e');
 
-router.get('/', (req, res, next) => {
-    request.post(utils.urlWrap('customers/5995a81dceb8abe24251849e')).send(req.body).end((err, returnedRes) => {
-        console.log(returnedRes);
+router.post('/', (req, res, next) => {
+    request.post(utils.urlWrap('accounts/5995b1f0ceb8abe2425184a5/purchases')).send(req.body).end((err, returnedRes) => {
+        res.json(returnedRes);
     });
 });
 
