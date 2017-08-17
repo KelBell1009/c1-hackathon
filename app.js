c51d5d7d9ecc0294ea4e24bb31f80833e63ebdac
Justin Kuang <zk65@cornell.edu>
2017-08-17 10:02:02 -0400

    removed unnecessary files, restart testing

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+
+var request = require('superagent');
+request.get('http://api.reimaginebanking.com/atms?key=4e7fe49f0f5ba28b8c28032a43551d7b').end(function (res) {
+    console.log(res);
+    // console.log(res.body); //do something
+    console.log("hello");
+});
