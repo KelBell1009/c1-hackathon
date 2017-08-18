736b64d429f0dc898d9ef0eafa989e2f42bfb206
David Van <David@DavidVan.net>
2017-08-17 23:20:05 -0400

    Fixed.

--- routes/makePurchase.js
+++ routes/makePurchase.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const utils = require('../nessieUtils').use('4e7fe49f0f5ba28b8c28032a43551d7b', '59963e18ceb8abe24251ae8c');
 
 router.post('/', (req, res, next) => {
-    request.post(utils.urlWrap('accounts/5995b1f0ceb8abe2425184a5/purchases')).send(req.body).end((err, returnedRes) => {
-        res.json(returnedRes);
+    request.get(utils.urlWrap('accounts/59963f42ceb8abe24251ae8e')).end((err, returnedRes) => {
+        console.log(returnedRes)
+        let body = req.body;
+        body.description = returnedRes.body.type + ',' + returnedRes.body.nickname + ',' + body.description;
+        request.post(utils.urlWrap('accounts/59963f42ceb8abe24251ae8e/purchases')).send(body).end((err, returnedRes2) => {
+            res.json(returnedRes2);
+        });
     });
 });
 
