b0a7a1789441602efa0002d1fe010086c03925b9
David Van <David@DavidVan.net>
2017-08-18 10:38:34 -0400

    Fixed.

--- public/js/statisticsjs.js
+++ public/js/statisticsjs.js
@@ -1,9 +1,6 @@
-document.getElementById('statistic-counter').innerHTML = Math.floor(Math.random() * 5).toString();
-$.get('/getSavings', function (data) {
-  console.log(data);
-  document.getElementById('movedSave').innerText = data.balance - 100;
+$.get('/getSavings', function(data) {
+  document.getElementById('movedSave').innerText = data.balance-100;
 });
-$.get('/getDonations', function (data) {
-  console.log(data);
+$.get('/getDonations', function(data) {
   document.getElementById('movedDonate').innerText = data.balance;
 });
