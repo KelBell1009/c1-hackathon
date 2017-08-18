b4e76bb5a7b3e51f4df97f6a8f8a099657eab350
Justin Kuang <zk65@cornell.edu>
2017-08-18 10:08:58 -0400

    random number gen

--- public/js/statisticsjs.js
+++ public/js/statisticsjs.js
@@ -1,8 +1,9 @@
-$.get('/getSavings', function(data) {
+document.getElementById('statistic-counter').innerHTML = Math.floor(Math.random() * 5).toString();
+$.get('/getSavings', function (data) {
   console.log(data);
-  document.getElementById('movedSave').innerText = data.balance-100;
+  document.getElementById('movedSave').innerText = data.balance - 100;
 });
-$.get('/getDonations', function(data) {
+$.get('/getDonations', function (data) {
   console.log(data);
   document.getElementById('movedDonate').innerText = data.balance;
 });
